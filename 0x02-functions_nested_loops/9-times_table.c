770a15214ebaa087235922f254f8ad5162098be1
Nyallu <anyallu2@gmail.com>
2022-09-14 14:14:50 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,56 +7,36 @@
  */
 
 void print_number(int n)
-
 {
 
 	int a, b;
 
-
-
 	if (n != 0)
-
 	{
 
 		if (n < 0)
-
 		{
-
 			_putchar('-');
-
 			n = -n;
-
 		}
-
 		a = n / 10;
-
 		b = n % 10;
-
 		if (a != 0)
 
 		{
-
 			print_number(a);
-
 			_putchar(b + '0');
 
 		}
-
 		else
-
 			_putchar(b + '0');
-
-
 
 	}
 
 	else
-
 		_putchar('0');
 
 }
-
-
 
 /**
  * times_table - prints times table up to 9 times
@@ -65,55 +45,37 @@ void print_number(int n)
  */
 
 void times_table(void)
-
 {
 
 	int i, j, k;
 
-
-
 	for (i = 0; i <= 9; i++)
-
 	{
-
 		_putchar('0');
 
 		for (j = 1; j <= 9; j++)
-
 		{
-
 			k = i * j;
-
 			if ((k > 9) && (k < 100))
 
 			{
-
 				_putchar(',');
-
 				_putchar(' ');
-
 				print_number(k);
 
 			}
 
 			else
-
 			{
 
 				_putchar(',');
-
 				_putchar(' ');
-
 				_putchar(' ');
-
 				_putchar(k + '0');
 
 			}
 
 		}
-
 		_putchar('\n');
-
 	}
-
 }
