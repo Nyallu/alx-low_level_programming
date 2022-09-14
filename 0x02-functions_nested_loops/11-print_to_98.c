7161b9ee939e5e82b2be7c93256523a9046032e7
Nyallu <anyallu2@gmail.com>
2022-09-14 12:34:21 -0700

    11print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-
 #include "main.h"
+
+void print_number(int n);
 
 
 
 /**
- * print_to_98 - This function prints all natural numbers
- * from n to 98, followed by a new line
- * @n: Starting integer to count down or up to 98
+ * print_to_98 - prints all integers from n to 98
+ * @n: input integer
+ *
+ * Return: void
  */
 
 void print_to_98(int n)
@@ -18,52 +19,112 @@ void print_to_98(int n)
 
 
 
-	if (n < 98)
+	if (n > 98)
 
 	{
 
-		for (i = n; i <= 98; i++)
+		print_number(n);
+
+		for (i = n - 1; i >= 98; i--)
 
 		{
 
-			if (i != 98)
+			_putchar(',');
 
-				printf("%i, ", i);
+			_putchar(' ');
 
-			else
-
-				printf("%i", i);
+			print_number(i);
 
 		}
 
 	}
 
-	else if (n > 98)
+	else if (n < 98)
 
 	{
 
-		for (i = n; i >= 98; i--)
+		print_number(n);
+
+		for (i = n + 1; i <= 98; i++)
 
 		{
 
-			if (i != 98)
+			_putchar(',');
 
-				printf("%i, ", i);
+			_putchar(' ');
 
-			else
-
-				printf("%i", i);
+			print_number(i);
 
 		}
+
+	}
+
+	else if (n == 98)
+
+	{
+
+		_putchar('9');
+
+		_putchar('8');
+
+	}
+
+	_putchar('\n');
+
+}
+
+/**
+ * print_number - prints out input
+ * @n: input
+ * Return: void
+ */
+
+void print_number(int n)
+
+{
+
+	int a, b;
+
+
+
+	if (n != 0)
+
+	{
+
+		if (n < 0)
+
+		{
+
+			_putchar('-');
+
+			n = -n;
+
+		}
+
+		a = n / 10;
+
+		b = n % 10;
+
+		if (a != 0)
+
+		{
+
+			print_number(a);
+
+			_putchar(b + '0');
+
+		}
+
+		else
+
+			_putchar(b + '0');
+
+
 
 	}
 
 	else
 
-		printf("%i", n);
-
-
-
-	printf("\n");
+		_putchar('0');
 
 }
