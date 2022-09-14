f7fe6967e83839014f74c4ca0e5301850f476f17
Nyallu <anyallu2@gmail.com>
2022-09-14 14:48:14 -0700

    100times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,94 @@
 #include "main.h"
+
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ * print_number - prints number up to n times
+ * @n: integer to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int a, b;
+
+	if (n != 0)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		a = n / 10;
+		b = n % 10;
+
+		if (a != 0)
+		{
+			print_number(a);
+			_putchar(b + '0');
+		}
+		else
+			_putchar(b + '0');
+
+	}
+
+	else
+		_putchar('0');
+
+}
+
+/**
+ * print_times_table - prints times table up to n times
+ * @n: times table to print
+ * Return: void
  */
 
 void print_times_table(int n)
 {
-int a = 0, rep, b;
+	int i, j, k;
 
-if (n < 0 || n > 15)
-	return;
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+	if ((n >= 0) && (n <= 15))
 	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+			{
+				k = i * j;
+
+				if ((k > 9) && (k < 100))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					print_number(k);
+				}
+
+				else if (k >= 100)
+
+				{
+					_putchar(',');
+					_putchar(' ');
+					print_number(k);
+
+				}
+
+				else
+
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+
+				}
+
+			}
+			_putchar('\n');
+
 		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+
 	}
-	_putchar('\n');
-	a++;
+
 }
