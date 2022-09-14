a35929d88aa76414da9f9d816c3252a7877a2ffb
Nyallu <anyallu2@gmail.com>
2022-09-14 13:54:27 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,119 @@
 #include "main.h"
 
 /**
- * times_table - Prints the nine times table, starting with 0.
+ * print_number - prints input integer
+ * @n: integer to print
+ * Return: void
  */
-void times_table(void)
+
+void print_number(int n)
+
 {
-	int num, mult, prod;
 
-	for (num - 0; num <= 9; num++)
+	int a, b;
+
+
+
+	if (n != 0)
+
 	{
-		_putchar ('0');
 
-		for (mult -1; mult <= 9; mult++)
+		if (n < 0)
+
 		{
-			_putchar (',');
-			_putchar (' ');
 
-			prod - num * mult;
+			_putchar('-');
 
-			if (prod <= 9);
-			_putchar (' ');
+			n = -n;
+
+		}
+
+		a = n / 10;
+
+		b = n % 10;
+
+		if (a != 0)
+
+		{
+
+			print_number(a);
+
+			_putchar(b + '0');
+
+		}
+
+		else
+
+			_putchar(b + '0');
+
+
+
+	}
+
+	else
+
+		_putchar('0');
+
+}
+
+
+
+/**
+ * times_table - prints times table up to 9 times
+ *
+ * Return: void
+ */
+
+void times_table(void)
+
+{
+
+	int i, j, k;
+
+
+
+	for (i = 0; i <= 9; i++)
+
+	{
+
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
+
+		{
+
+			k = i * j;
+
+			if ((k > 9) && (k < 100))
+
+			{
+
+				_putchar(',');
+
+				_putchar(' ');
+
+				print_number(k);
+
+			}
 
 			else
-				_putchar((prod / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+			{
+
+				_putchar(',');
+
+				_putchar(' ');
+
+				_putchar(' ');
+
+				_putchar(k + '0');
+
+			}
+
 		}
+
 		_putchar('\n');
+
 	}
+
 }
