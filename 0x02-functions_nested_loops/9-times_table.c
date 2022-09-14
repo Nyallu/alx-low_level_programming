baa63227a1f959661e9c5c790037783e70767103
Nyallu <anyallu2@gmail.com>
2022-09-14 12:28:12 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,92 @@
-#include <unistd.h>
-
 #include "main.h"
 
 
 
 /**
- * times_table - This function prints the 9 times table
- * starting with 0
+ * print_number - prints input integer
+ * @n: integer to print
+ * Return: void
+ */
+
+void print_number(int n)
+
+{
+
+	int a, b;
+
+
+
+	if (n != 0)
+
+	{
+
+		if (n < 0)
+
+		{
+
+			_putchar('-');
+
+			n = -n;
+
+		}
+
+		a = n / 10;
+
+		b = n % 10;
+
+		if (a != 0)
+
+		{
+
+			print_number(a);
+
+			_putchar(b + '0');
+
+		}
+
+		else
+
+			_putchar(b + '0');
+
+
+
+	}
+
+	else
+
+		_putchar('0');
+
+}
+
+
+
+/**
+ * times_table - prints times table up to 9 times
+ *
+ * Return: void
  */
 
 void times_table(void)
 
 {
-	int x, y, a, b;
 
-	for (x = 0; x < 10; x++)
+	int i, j, k;
+
+
+
+	for (i = 0; i <= 9; i++)
 
 	{
 
-		for (y = 0; y < 10; y++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 
 		{
 
-			a = (x * y) / 10;
+			k = i * j;
 
-			b = (x * y) % 10;
-
-
-
-			if ((x * y) > 9)
-
-				_putchar(a + '0');
-
-
-			_putchar(b + '0');
-
-
-			/* Don't print commas after the 10th column */
-
-			if (y != 9)
+			if ((k > 9) && (k < 100))
 
 			{
 
@@ -46,21 +94,26 @@ void times_table(void)
 
 				_putchar(' ');
 
+				print_number(k);
+
 			}
 
+			else
 
+			{
 
-			/* Add an extra space if printing single digits */
-
-			if (y == 9 && x == 0)
-
-				break;
-
-			else if (x * (y + 1) < 10)
+				_putchar(',');
 
 				_putchar(' ');
 
+				_putchar(' ');
+
+				_putchar(k + '0');
+
+			}
+
 		}
+
 		_putchar('\n');
 
 	}
