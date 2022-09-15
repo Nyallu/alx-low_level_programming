df9407e3bea7833b13cc02e0dc1ca27a99fae34a
Nyallu <anyallu2@gmail.com>
2022-09-15 15:34:19 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,26 +8,6 @@
 
 void print_times_table(int n)
 {
-	{
-
-		    print_times_table(3);
-
-		        _putchar('\n');
-
-			    print_times_table(5);
-
-			        _putchar('\n');
-
-				    print_times_table(98);
-
-				        _putchar('\n');
-
-					    print_times_table(12);  
-
-					        return (0);
-
-	}
-
 	int x = 0, y, z;
 
 	if (n > 15 || n < 0)
