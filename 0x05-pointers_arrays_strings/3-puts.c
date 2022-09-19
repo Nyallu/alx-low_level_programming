a5140009b5c7f66f69e9078585065722b0715093
Nyallu <anyallu2@gmail.com>
2022-09-19 13:44:44 -0700

    done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,43 @@
 #include "main.h"
 
 /**
- * _puts - write string followed by new line
- * @str: string
- * Return: void
+ * _strlen - returns the length of a string.
+ * @s: pointer to String
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+
+		i++;
+
+	}
+
+	return (i);
+}
+
+/**
+ * _puts - prints to string
+ * @str: Strin to print
+ * Return: Nothing
  */
 
 void _puts(char *str)
 {
-	while (*str)
+	int i = 0, strln = 0;
 
-		_putchar(*str++);
+	strln = _strlen(str);
+	for ( ; i <= (strln - 1); i++)
+	{
+
+		_putchar(*(str + i));
+
+	}
 
 	_putchar('\n');
 }
