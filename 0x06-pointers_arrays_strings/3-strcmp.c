83413d378b5e586d887d210cb820f69d8196c795
Nyallu <anyallu2@gmail.com>
2022-09-21 18:38:23 -0700

    Function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,6 @@
 
 int _strcmp(char *s1, char *s2)
 {
-
 	int a = 0, b = 0, c = 0, r = 0, lim;
 
 	while (s1[a])
@@ -35,15 +34,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (c <= lim)
 	{
-
 		if (s1[c] == s2[c])
 		{
 			c++;
 			continue;
 		}
-
 		else
-
 		{
 			r = s1[c] - s2[c];
 			break;
