49806e49f3b26bf74d2618d1bd7d7bb3ab51698d
Nyallu <anyallu2@gmail.com>
2022-09-22 11:44:40 -0700

    To dd one line so that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,14 +2,15 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
 
-	a[2] = 1024;
-	p = &n;
+  int n;
+  int a[5];
+  int *p;
 
-/**
+  a[2] = 1024;
+  p = &n;
+
+  /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
@@ -20,9 +21,10 @@ int main(void)
 
   *(p + 5) = 98;
 
-	/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 
-	printf("a[2] = %d\n", a[2]);
+  printf("a[2] = %d\n", a[2]);
 
-	return (0);
+  return (0);
+
 }
